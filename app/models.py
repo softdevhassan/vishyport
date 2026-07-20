ed4f2dffda38ccfca3d60d8e0aebf7d926691a03
softdevhassan <softdevhassan@gmail.com>
2026-07-20 05:38:08 +0500

    Fixes

--- app/models.py
+++ app/models.py
@@ -1,21 +1,4 @@
-from app import db, login_manager
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+from app import db
 
 class SiteSettings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
